bf5d35035f3a6b166a8e013c2f6fc73698d30484
Andrei1694 <andrei4.stanciu@gmail.com>
2019-01-23 12:40:51 +0200

    Initial commit

---
+++ src/main/java/com/stanciuandrei/spring5petclinic/Spring5PetclinicApplication.java
@@ -0,0 +1,14 @@
+package com.stanciuandrei.spring5petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5PetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5PetclinicApplication.class, args);
+    }
+
+}
+
