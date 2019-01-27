8fd1242dfee12a48634bb8651fe77f69698ca087
Andrei1694 <andrei4.stanciu@gmail.com>
2019-01-27 22:25:11 +0200

    adding POJOs. Closes #2.

---
+++ src/main/java/com/stanciuandrei/spring5petclinic/Model/Owner.java
@@ -0,0 +1,5 @@
+package com.stanciuandrei.spring5petclinic.Model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/com/stanciuandrei/spring5petclinic/Model/Vet.java
@@ -0,0 +1,4 @@
+package com.stanciuandrei.spring5petclinic.Model;
+
+public class Vet extends Person{
+}
